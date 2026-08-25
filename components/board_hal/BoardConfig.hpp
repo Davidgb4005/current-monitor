d887adcb5e8e3cf3b82b39e3215e5de452d659b1
Dave <dave@debian.Dave>
2026-08-24 22:27:32 -0600

    commit new hal naming

--- components/board_hal/BoardConfig.hpp
+++ components/board_hal/BoardConfig.hpp
@@ -27,7 +27,7 @@ constexpr uint8_t kAlternatorCurrentChannel = 2;
 constexpr uint8_t kAlternatorVoltageChannel = 0;
 constexpr uint8_t kPassengerVoltageChannel = 4;
 constexpr uint8_t kDriverVoltageChannel = 5;
-constexpr uint8_t kDriverCurrentChannel = 1;
-constexpr uint8_t kPassengerCurrentChannel = 3;
+constexpr uint8_t kDriverCurrentChannel = 3;
+constexpr uint8_t kPassengerCurrentChannel = 1;
 
 }  // namespace board
